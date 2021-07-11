a49e2ae00bb923623ea5a5e0ef1509a5fb6ed265
Łukasz Sitarski <lucassith@gmail.com>
2021-07-11 20:54:59 +0200

    update

--- app.js
+++ app.js
@@ -13,6 +13,12 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', indexRouter);
 app.options('/cors', cors({
   "origin": "https://cors.3lancers.dev",
   "allowedHeaders": ["Access-Control-Allow-Origin"],
@@ -21,12 +27,6 @@ app.options('/cors', cors({
   "optionsSuccessStatus": 204
 }))
 app.options('/no-cors', cors())
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
